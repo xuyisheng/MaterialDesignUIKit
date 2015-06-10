4a9834a9200b93a398b7536d61619b441bcd8aac
xuyisheng <xuyisheng@hujiang.com>
2015-06-10 14:24:53 +0800

    Initial commit

---
+++ app/src/main/java/com/xys/materialdesignuikit/DetailActivity.java
@@ -0,0 +1,28 @@
+package com.xys.materialdesignuikit;
+
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        Toolbar toolbar = (Toolbar) this.findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle("我的课程");
+    }
+
+    public void checkin(View view) {
+        Snackbar.make(view, "checkin success!", Snackbar.LENGTH_SHORT).show();
+    }
+}
